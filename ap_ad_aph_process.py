4e6dc72ac846c90e519cca2038aa0445bf61281d
lifeodyssey <zhouthepassion@outlook.com>
2021-03-09 19:51:13 +0900

    Processing 2016 0824

--- ap_ad_aph_process.py
+++ ap_ad_aph_process.py
@@ -4,26 +4,26 @@ import scipy.optimize
 import pandas as pd
 from IADP import ap_ad_aph
 
-ap_path = '/Users/zhenjia/Desktop/Airka_Reprocess/Ariake/2016/2016.03.08/ap/'
-ad_path = '/Users/zhenjia/Desktop/Airka_Reprocess/Ariake/2016/2016.03.08/ad/'
+ap_path = '/Users/zhenjia/Desktop/Airka_Reprocess/Ariake/2016/2016.08.24/IOP-20160824/ap/'
+ad_path = '/Users/zhenjia/Desktop/Airka_Reprocess/Ariake/2016/2016.08.24/IOP-20160824/ad/'
 # output_path = '/Users/zhenjia/Desktop/Airka_Reprocess/Ariake/2015/2015.09.16/'
 
 AP_S1 = {'Sample': ['C0308S11', 'C0308S12'],
          'Before Blank': ['B0308_B1', 'A0308_B2'],
          'After Blank': ['C0308B3', 'C0308B4'],
-         'Vol': 150.20,
+         'Vol': 150,
          'Diameter': [22.07, 22.22, 22.37]}
 
 AP_S2 = {'Sample': ['C0308S21', 'C0308S22'],
          'Before Blank': ['C0308B3', 'C0308B4'],
          'After Blank': ['C0308B5', 'C0308B6'],
-         'Vol': 150.50,
+         'Vol': 150,
          'Diameter': [22.63, 22.41, 22.56]}
 
 AP_S3 = {'Sample': ['C0308S31', 'C0308S32'],
          'Before Blank': ['C0308B7', 'C0308B8'],
          'After Blank': ['C0308B9', 'C308B10'],
-         'Vol': 150.30,
+         'Vol': 150,
          'Diameter': [22.54, 22.09, 22.22]}
 
 AP_S4 = {'Sample': ['C0308S41', 'C0308S42'],
@@ -41,32 +41,33 @@ AP_S5 = {'Sample': ['C0308S52', 'C0308S52'],
 AP_S6 = {'Sample': ['C0308S61', 'C0308S62'],
          'Before Blank': ['C0308B9', 'C308B10'],
          'After Blank': ['C308B11', 'C308B12'],
-         'Vol': 100,
+         'Vol': 150,
          'Diameter': [22.71, 22.67, 22.38]}
 
 AP_S7 = {'Sample': ['C308S71', 'C0308S72'],
          'Before Blank': ['C308B21', 'C308B22'],
          'After Blank': ['C308B31', 'C308B32'],
-         'Vol': 200,
+         'Vol': 150,
+         'Diameter': [21.98, 22.08, 22.39]}
+AP_S8 = {'Sample': ['C308S71', 'C0308S72'],
+         'Before Blank': ['C308B21', 'C308B22'],
+         'After Blank': ['C308B31', 'C308B32'],
+         'Vol': 150,
          'Diameter': [21.98, 22.08, 22.39]}
 
 AP_S9 = {'Sample': ['C0308S91', 'C0308S92'],
          'Before Blank': ['C308B21', 'C308B22'],
          'After Blank': ['C308B31', 'C308B32'],
-         'Vol': 100,
+         'Vol': 150,
          'Diameter': [22.42, 22.22, 22.18]}
 
-AP_S10 = {'Sample': ['C308S101', 'C308S102'],
+AP_S12 = {'Sample': ['C308S101', 'C308S102'],
           'Before Blank': ['C308B21', 'C308B22'],
           'After Blank': ['C308B31', 'C308B32'],
           'Vol': 150,
           'Diameter': [22.17, 22.30, 22.24]}
 
-AP_S11 = {'Sample': ['C308S111', 'C308S112'],
-          'Before Blank': ['C308B21', 'C308B22'],
-          'After Blank': ['C308B31', 'C308B32'],
-          'Vol': 100,
-          'Diameter': [22.35, 22.29, 22.04]}
+
 
 AD_S1 = {'Sample': ['D0308S11', 'D0308S12'],
          'Before Blank': ['D0308B11', 'D0308B12'],
@@ -95,21 +96,38 @@ AD_S6 = {'Sample': ['D0308S61', 'D0308S62'],
 AD_S7 = {'Sample': ['D0308S71', 'D0308S72'],
          'Before Blank': ['D0308B31', 'D0308B32'],
          'After Blank': ['D0308B51', 'D0308B52'], }
-#
-# AD_S8 = {'Sample': ['D0308S81', 'D0308S82'],
-#          'Before Blank': ['D0308B72', 'D0308B72'],
-#          'After Blank': ['D0308B82', 'D0308B82'], }
+
+AD_S8 = {'Sample': ['D0308S81', 'D0308S82'],
+         'Before Blank': ['D0308B72', 'D0308B72'],
+         'After Blank': ['D0308B82', 'D0308B82'], }
 
 AD_S9 = {'Sample': ['D0308S91', 'D0308S92'],
          'Before Blank': ['D0308B61', 'D0308B62'],
          'After Blank': ['D0308B71', 'D0308B72'], }
 
-AD_S10 = {'Sample': ['D308S101', 'D308S102'],
+AD_S12 = {'Sample': ['D308S101', 'D308S102'],
           'Before Blank': ['D0308B61', 'D0308B62'],
           'After Blank': ['D0308B71', 'D0308B72'], }
-AD_S11 = {'Sample': ['D308S111', 'D308S112'],
-          'Before Blank': ['D0308B61', 'D0308B62'],
-          'After Blank': ['D0308B71', 'D0308B72'], }
+
+
+'''
+顺序
+
+Station ISZK
+ 1        1
+ 2        2
+ 3        3
+ 4        4
+ 5        5
+ 6        12
+ 7        6
+ 8        9
+ 9        7
+ 10       8
+ 前面标记的StationXXX实际上是ISZK XXX
+ 表里的顺序是按照ISZK的上面的那个顺序（而非标号的顺序）来的
+'''
+
 
 ap_list = [
     AP_S1,
@@ -117,11 +135,12 @@ ap_list = [
     AP_S3,
     AP_S4,
     AP_S5,
+    AP_S12,
     AP_S6,
-    AP_S7,
     AP_S9,
-    AP_S10,
-    AP_S11,
+    AP_S7,
+    AP_S12,
+    AP_S8,
 ]
 
 ad_list = [
